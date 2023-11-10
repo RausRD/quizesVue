4830549deb6a21378159a365350233c4f0778344
Ruslan Dunets <dunecruslan2@gmail.com>
2023-11-10 08:02:42 +0200

    refactor code

---
+++ src/components/Question.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="question-container">
+		<h1 class="question">vs sdvsd dssdvv dskjdsvnv</h1>
+	</div>
+	<div class="options-container">
+		<div class="option">
+			<p class="option-label">A</p>
+			<div class="option-value">
+				<p>NaCL</p>
+			</div>
+		</div>
+		<div class="option">
+			<p class="option-label">A</p>
+			<div class="option-value">
+				<p>NaCL</p>
+			</div>
+		</div>
+		<div class="option">
+			<p class="option-label">A</p>
+			<div class="option-value">
+				<p>NaCL</p>
+			</div>
+		</div>
+		<div class="option">
+			<p class="option-label">A</p>
+			<div class="option-value">
+				<p>NaCL</p>
+			</div>
+		</div>
+	</div>
+</template>
+<script setup>
+</script>
+
+<style scoped>
+.options-container {
+	margin-top: 20px;
+}
+.question {
+	font-size: 40px;
+	margin-bottom: 20px;
+}
+.option {
+	display: flex;
+	margin-bottom: 20px;
+	cursor: pointer;
+}
+.option-label {
+	background-color: bisque;
+	width: 50px;
+	height: 50px;
+	font-size: 30px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+.option-value {
+	background-color: rgb(244, 239, 239);
+	width: 100%;
+	font-size: 30px;
+	padding: 0 20px;
+}
+</style>
